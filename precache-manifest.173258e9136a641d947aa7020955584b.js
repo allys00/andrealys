053a51d220b12b7206dcd70b97a179991cba9e49
André Alys <andreallys2014@gmail.com>
2019-04-27 20:11:09 -0300

    Updates

---
+++ precache-manifest.173258e9136a641d947aa7020955584b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6e68c2379ebaa5daf84",
+    "url": "/andrealys/static/css/main.50bbfaa1.chunk.css"
+  },
+  {
+    "revision": "f6e68c2379ebaa5daf84",
+    "url": "/andrealys/static/js/main.8b216140.chunk.js"
+  },
+  {
+    "revision": "5f482569774576f4e1c7",
+    "url": "/andrealys/static/js/runtime~main.8b7f0d56.js"
+  },
+  {
+    "revision": "b2d5e939d515d87a64d4",
+    "url": "/andrealys/static/js/2.aeaa381a.chunk.js"
+  },
+  {
+    "revision": "d839188883f1d9f624337537c91f8360",
+    "url": "/andrealys/static/media/hero.d8391888.png"
+  },
+  {
+    "revision": "8e6071d7f039713e3be07204251bae1c",
+    "url": "/andrealys/static/media/myAvatar.8e6071d7.svg"
+  },
+  {
+    "revision": "d619cfc0b2eb761f04a42e03b1ecbdf5",
+    "url": "/andrealys/static/media/react.d619cfc0.jpg"
+  },
+  {
+    "revision": "fb525f674995356e3a717442902f664c",
+    "url": "/andrealys/static/media/ionic3.fb525f67.jpg"
+  },
+  {
+    "revision": "a79c69c98ec6daafb8d24d99130ac230",
+    "url": "/andrealys/static/media/desenvolvimento-web.a79c69c9.jpg"
+  },
+  {
+    "revision": "f03b58c510b9fe4bc5bd09d22f0a4740",
+    "url": "/andrealys/static/media/eurostile-bold.f03b58c5.otf"
+  },
+  {
+    "revision": "50079efd5fcba914a24094d999b0bf8e",
+    "url": "/andrealys/index.html"
+  }
+];
